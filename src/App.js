4ccba38a69b5b0c7a35239f786cff8a79d44c9a5
Emily Landgrebe <emilandgrebe@gmail.com>
2019-10-30 15:22:26 -0500

    create getWeather async function

--- src/App.js
+++ src/App.js
@@ -13,11 +13,34 @@ class App extends React.Component {
     description: undefined,
     error: undefined
   }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.REACT_APP_API_KEY}`)
+    const response = await api_call.json()
+    this.setState=({
+      temperature: Math.trunc((response.main.temp - 273.15)*(1.8)+32),
+      tempLow: Math.trunc((response.main.temp_min - 273.15)*(1.8)+32),
+      tempHigh: Math.trunc((response.main.temp_max - 273.15)*(1.8)+32),
+      city: response.name,
+      country: response.sys.country,
+      humidity: response.main.humidity,
+      windSpeed: response.wind.speed,
+      description: response.weather[0].description,
+      error: ""
+    })
+    console.log(response, this.state.windSpeed)
+  }
+
   render() {
     return (
       <div className="App">
         <Titles />
-        <Form />
+        <Form
+          loadWeather={this.getWeather}
+        />
       </div>
     )
   }
